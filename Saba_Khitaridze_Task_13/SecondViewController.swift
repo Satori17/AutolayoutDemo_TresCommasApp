0016ba723632a115c40b0b9d6c587d144032e5ba
saba khitaridze <sabaxitara@icloud.com>
2022-07-05 13:42:33 +0400

    Design changes

---
+++ Saba_Khitaridze_Task_13/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Saba_Khitaridze_Task_13
+//
+//  Created by Saba Khitaridze on 05.07.22.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    @IBOutlet weak var saveButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        saveButton.layer.cornerRadius = 25
+        
+    }
+
+}
--- Saba_Khitaridze_Task_13/ViewController.swift
+++ Saba_Khitaridze_Task_13/ViewController.swift
@@ -9,9 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var signUpButton: UIButton!
+    @IBOutlet weak var logInButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        signUpButton.layer.cornerRadius = 25
+        logInButton.layer.cornerRadius = 25
     }
 
 
